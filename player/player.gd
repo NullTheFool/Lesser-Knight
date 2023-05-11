2766c55e1f7a148ff5ea4fcb7fb8cd9943b31d08
NullTheFool <douglaschavez542@gmail.com>
2023-05-10 23:27:09 -0600

    El proyecto se habia corrompido, pero por suerte se pudo recuperar.
    El sprite del caballero, y su animacion de idle han sido
    añadidas.

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -250.0
-
+@export var jump_velocity: float 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var speed: float = 200.0
+var has_armor:bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,16 +14,22 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	if Input.is_action_just_pressed("ui_down"):
+		jump_velocity = -350.0
+		has_armor = not has_armor
+		if has_armor:
+			jump_velocity = -250.0
+
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
