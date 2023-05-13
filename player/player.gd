542b0d531f92a7a47ad6b2951d10eb635daaf500
NullTheFool <douglaschavez542@gmail.com>
2023-05-13 16:27:24 -0600

    - Nuevas animaciones del caballero añadidas. - Nuevo tileset (WIP).

--- player/player.gd
+++ player/player.gd
@@ -23,6 +23,7 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
+		animated_sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
